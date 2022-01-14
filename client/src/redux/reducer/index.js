af926ab460998345482a8e62555de06953076d5f
Victor Buesaquillo <vicbfisico@gmail.com>
2022-01-14 18:27:34 -0500

    Add(src) redux install

---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+
+
+
+        default:
+            return state;
+    }
+}
