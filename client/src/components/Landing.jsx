1c31f7637ee98a50be8c90500c5a127c6991edd7
mariorocha23 <virgoroch852@gmail.com>
2022-01-18 08:44:47 -0600

    add(home) render catalogue cards

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -5,9 +5,7 @@ const Landing = () => {
     return (
         <div className="landing">
             <div className='landing__text'>
-                <p className='landing__text--in'>Find interesting works of art</p>
-                <p className='landing__text--midle'>in Museum </p>
-                <p className='landing__text--end'>and take them with you</p>
+                <p className='landing__text--in'>Find interesting works of art in Museum and take them with you</p>
             </div>
             <div className='landing__image'>
                 <img src={landing} width="50%" alt="#" className="image__image"></img>
