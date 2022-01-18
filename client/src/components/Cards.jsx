bdc87b9d520011b86c3d1cc74a14c4fed92f422f
Victor Buesaquillo <vicbfisico@gmail.com>
2022-01-17 19:27:29 -0500

    Add(components landing)

---
+++ client/src/components/Cards.jsx
@@ -0,0 +1,11 @@
+import './styles/Cards.css'
+
+const Cards = () => {
+    return (<div className='cards'>
+        <img src="https://www.publicdomainpictures.net/pictures/370000/nahled/skeletons-in-museum-1602841442dSz.jpg"
+            width="100%" alt="#" ></img>
+    </div>);
+}
+
+export default Cards;
+
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import TopBar from '../components/TopBar'
 import Landing from '../components/Landing'
 import './styles/Home.css'
+import Cards from '../components/Cards';
+
 const Home = () => {
     return (<div className='home'>
         <TopBar />
         <Landing />
+        <Cards />
     </div>);
 }
 
