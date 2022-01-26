3aec69040a6a06e164c1bad6b02d6f15c418d77b
Victor Buesaquillo <vicbfisico@gmail.com>
2022-01-26 14:05:19 -0500

    Refactor(components TopBar.jsx) add autocomplete search

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -1,25 +1,21 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import { AiOutlineSearch } from 'react-icons/ai';
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import "./styles/TopBar.css";
 import { NavLink } from "react-router-dom"
-import { AiOutlineSearch } from 'react-icons/ai';
 import { MdShoppingBasket } from 'react-icons/md';
 import { getFindGallery, getAllGallery } from '../redux/actions/galleryActions'
 import SortInput from "./SortInput";
-
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
 function TopBar() {
-
     const dispatch = useDispatch()
     const total = useSelector(state => state.allProductsReducer.totalCount)
+    const items = useSelector(state => state.galleryReducer.allGallery)
     const [input, setInput] = useState({
         search: ''
     })
-
-    const handleSearch = (e) => {
-        setInput({ ...input.search, search: e.target.value })
-    }
 
     useEffect(() => {
         if (input.search) {
@@ -30,14 +26,40 @@ function TopBar() {
         }
     }, [dispatch, input.search, total])
 
+    const handleOnSearch = (string, results) => {
+        setInput({ ...input.search, search: string })
+    }
+
+    const handleOnSelect = (item) => {
+        setInput({ ...input.search, search: item.title })
+    }
+
+    const handleOnClear = () => {
+        dispatch(getAllGallery())
+    }
+
     return (
         <div className="topbar">
             <div className="topbar__content">
                 <div className="searchForm">
-                    <AiOutlineSearch className="searchForm__icon" />
-                    <input className="search__input" placeholder="Search here..." onChange={handleSearch} value={input.search} />
+                    <div >
+                        <ReactSearchAutocomplete
+                            items={items}
+                            className="search__input"
+                            placeholder="Search here..."
+                            onSearch={handleOnSearch}
+                            onSelect={handleOnSelect}
+                            onClear={handleOnClear}
+                            value={input.search}
+                            fuseOptions={{ keys: ["title", "description"] }} // Search on both fields
+                            resultStringKeyName="title" // String to display in the results
+                            showIcon={true}
+                        // showClear={true}
+                        />
+                        <div style={{ width: "200px" }}></div>
+                    </div>
                 </div>
-                  <SortInput/>
+                <SortInput />
                 <div className='cart'>
                     <div className='cart__content'>
                         <MdShoppingBasket className='cart__icon' />
@@ -45,15 +67,9 @@ function TopBar() {
                     </div>
                 </div>
                 <div className='loginButtons'>
-
-                    {/* <button className='loginButtons__button'>
-                        <NavLink to="/Login">Login</NavLink>
-                    </button> */}
-
                     <button className='loginButtons__button'>
                         <NavLink to="/signup">Create Account</NavLink>
                     </button>
-
                 </div>
 
             </div>
