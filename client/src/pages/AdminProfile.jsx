736bd8813c43c28bf1ef551d29e6df8093953758
mariorocha23 <virgoroch852@gmail.com>
2022-01-27 09:26:57 -0600

    add(deleteUserInDataBase) one user can be deleted your account

--- client/src/pages/AdminProfile.jsx
+++ client/src/pages/AdminProfile.jsx
@@ -3,40 +3,66 @@ import { useEffect } from 'react';
 import image from '../images/signup.svg'
 import Cookies from 'universal-cookie';
 import {CgProfile} from 'react-icons/cg';
+import Swal from 'sweetalert2'
 
 const cookies = new Cookies();
 
 function AdminProfile(){
 
     const user = cookies.get('session')
-    // const user ={
-    //     email: "blueowl2794@gmail.com",
-    //     familyName: "Olarte Molina",
-    //     givenName: "Erik Santiago",
-    //     googleId: "108572354537016537304",
-    //     imageUrl: image,
-    //     name: "Erik Santiago Olarte Molina",
-    // };
 
     useEffect( ()=> {
         if(!cookies.get("session")){
             window.location.href = "./login";
-        } 
-    }) 
+        }
+    })
 
     const handleDeleteUser = ()=>{
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover your account!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+            showCancelButton: true,
+          })
+          .then((willDelete) => {
+            if (willDelete.isConfirmed) {
+
+                fetch(`http://localhost:5040/users/${user.id}`,{
+                    method: 'DELETE',
+                    mode: 'cors',
+                    headers: {
+                            'x-access-token': `${user.accessToken}`,
+                            'Content-Type': 'application/json',
+                        },
+                    }).then(response => {
+                        console.log(response);
+                        cookies.remove('session')
+                        Swal.fire("Poof! Your account has been deleted successfully!", {
+                            icon: "success",
+                        }).then(reload => { window.location.href = '/'})
+
+                    }).catch(error => {
+                            console.log(error);
+                    })
+            } else {
+              return null
+            }
+        });
     }
-    
+
 
     return(
         <div className="admin-profile">
             <div className="admin-profile-top">
                 <div>
                     <h1>Overview</h1>
-                </div> 
+                </div>
                 <div className="admin-profile-top-info">
                     <div >
-                        <h5>{cookies.get('email')}</h5>
+                        <h5>{user.email}</h5>
                         <button className="admin-profile-top-info-butt" onClick={handleDeleteUser}> Eliminar cuenta</button>
                     </div>
                     <CgProfile/>
@@ -56,7 +82,7 @@ function AdminProfile(){
                     <div className="admin-box">
                         <section className="admin-box-at">
                             <b>...</b>
-                            
+
                             <br />
                             <br />
                             <br />
@@ -65,13 +91,13 @@ function AdminProfile(){
                         </section>
                         <section className="admin-box-at">
                             <b>ordenes</b>
-                            
+
                             <br />
                             <br />
                             <br />
                             <br />
                             <br />
-                        </section> 
+                        </section>
                     </div>
                 </div>
                 <div className="admin-boxTwo">
