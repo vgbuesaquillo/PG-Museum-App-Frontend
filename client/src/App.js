183759b568cbb49a00727dbfac2b8697bfd35a74
Jorge Luis Gonzalez Vargas <otalumi@hotmail.com>
2022-01-28 16:38:08 -0600

    admin/new, store component reutilizado para admin/product-list

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import GitHub from './pages/GitHub';
 import Detail from './pages/Detail.jsx';
 import Store from './pages/Store';
 import AdminProfile from './pages/AdminProfile';
+import NewProduct from './components/adminPage/NewProduct';
 
 function App() {
   return (
@@ -21,8 +22,13 @@ function App() {
         <Route path="/favorite" element={<> <NavBar /><Favorite /></>} />
         <Route path="/setting" element={<> <NavBar /><Setting /></>} />
         <Route path="/github" element={<> <NavBar /><GitHub /></>} />
+        
         <Route path="/Admin" element={<> <NavBar /><AdminProfile /></>} />
-        <Route path="/store" element={<> <NavBar /><Store /></>} />
+        <Route path="/Admin/new" element={<> <NavBar /><NewProduct/> </>} />
+        <Route path="/Admin/product-list" element={<> <NavBar /><Store reducer={"allProductsReducer"} property={"allproducts"} title={"Product List"} /> </>} />
+        
+
+        <Route path="/store" element={<> <NavBar /><Store reducer={"allProductsReducer"} property={"allproducts"} title={"My Cart"} /></>} />
         <Route path="/:id" element={<> <NavBar /><Detail /></>} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
--- client/src/pages/AdminProfile.jsx
+++ client/src/pages/AdminProfile.jsx
@@ -2,91 +2,81 @@ import './styles/AdminProfile.css';
 import { useEffect } from 'react';
 import image from '../images/signup.svg'
 import Cookies from 'universal-cookie';
-import {CgProfile} from 'react-icons/cg';
+import { CgProfile } from 'react-icons/cg';
+import { Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
 const cookies = new Cookies();
 
-function AdminProfile(){
+function AdminProfile() {
 
-    const user = cookies.get('session')
-    // const user ={
-    //     email: "blueowl2794@gmail.com",
-    //     familyName: "Olarte Molina",
-    //     givenName: "Erik Santiago",
-    //     googleId: "108572354537016537304",
-    //     imageUrl: image,
-    //     name: "Erik Santiago Olarte Molina",
-    // };
+  const user = cookies.get('session')
+  // const user ={
+  //     email: "blueowl2794@gmail.com",
+  //     familyName: "Olarte Molina",
+  //     givenName: "Erik Santiago",
+  //     googleId: "108572354537016537304",
+  //     imageUrl: image,
+  //     name: "Erik Santiago Olarte Molina",
+  // };
 
-    useEffect( ()=> {
-        if(!cookies.get("session")){
-            window.location.href = "./login";
-        } 
-    }) 
-
-    const handleDeleteUser = ()=>{
+  useEffect(() => {
+    if (!cookies.get("session")) {
+      window.location.href = "./login";
     }
-    
+  })
 
-    return(
-        <div className="admin-profile">
-            <div className="admin-profile-top">
-                <div>
-                    <h1>Overview</h1>
-                </div> 
-                <div className="admin-profile-top-info">
-                    <div >
-                        <h5>{cookies.get('email')}</h5>
-                        <button className="admin-profile-top-info-butt" onClick={handleDeleteUser}> Eliminar cuenta</button>
-                    </div>
-                    <CgProfile/>
-                </div>
-            </div>
-            <div className="admin-profile-bot">
-                <div className="admin-boxOne">
-                    <div className="admin-info">
-                        <section className="admin-info-at">
-                            <h4>informacion</h4>
-                            <b>Username {' ' + user.username}</b>
-                            <b>correo {' ' + user.email}</b>
-                            <b>Tipo: {' ' + user.roles}</b>
-                            {/* <b>Fecha de registro: {' ' + cookies.get('registro')}</b> */}
-                        </section>
-                    </div>
-                    <div className="admin-box">
-                        <section className="admin-box-at">
-                            <b>...</b>
-                            
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                        </section>
-                        <section className="admin-box-at">
-                            <b>ordenes</b>
-                            
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                        </section> 
-                    </div>
-                </div>
-                <div className="admin-boxTwo">
-                    <section>
-                        <h4>historial</h4>
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                    </section>
-                </div>
-            </div>
+  const handleDeleteUser = () => {
+  }
+
+
+  return (
+    <div className="admin-profile">
+      <div className="admin-profile-top">
+        <div>
+          <h1>Overview</h1>
         </div>
-    )
+        <div className="admin-profile-top-info">
+          <div >
+            <h5>{cookies.get('email')}</h5>
+            <button className="admin-profile-top-info-butt" onClick={handleDeleteUser}> Eliminar cuenta</button>
+          </div>
+          <CgProfile />
+        </div>
+      </div>
+      <div className="admin-profile-bot">
+        <div className="admin-boxOne">
+          <div className="admin-info">
+            <section className="admin-info-at">
+              <h4>informacion</h4>
+              <b>Username: {user.username}</b>
+              <b>correo: {user.email}</b>
+              <b>Tipo: {user.roles}</b>
+              <div>
+                <Link to={'new'}> <Button secondary>New Product</Button></Link>
+                <Link to={'product-list'}> <Button secondary>Product List</Button></Link>
+              </div>
+            </section>
+          </div>
+          <div className="admin-box">
+            <section className="admin-box-at">
+
+            </section>
+            <section className="admin-box-at">
+              <b>ordenes</b>
+
+            </section>
+          </div>
+        </div>
+        <div className="admin-boxTwo">
+          <section>
+            <h4>historial</h4>
+
+          </section>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AdminProfile;
