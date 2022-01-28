bcf1ab5a1ae3c5796cf7b0029f49251145303db8
Victor Buesaquillo <vicbfisico@gmail.com>
2022-01-28 09:01:46 -0500

    Add(pages utils) resetpassword put and post, refactor login and app

---
+++ client/src/utils/validatePassword.js
@@ -0,0 +1,17 @@
+export default function validatePassword(user) {
+    let errors = {};
+    if (!user.password) {
+        errors.password = "Password is required"
+    } else if (!/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/.test(user.password)) {
+        errors.password = "Password need to be eight characters or more; must have at least one digit, one special symbol or one uppercase character"
+    }
+
+    if (!user.passwordbis) {
+        errors.passwordbis = "Password is required"
+    } else if (user.password !== user.passwordbis) {
+        errors.passwordbis = "Passwords are not the same"
+    }
+
+    return errors;
+
+}
