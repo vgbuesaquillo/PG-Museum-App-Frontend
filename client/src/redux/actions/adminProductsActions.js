8a13cd7dd579e38ed2a7fabf39df80dda4d2f4f5
Jorge Luis Gonzalez Vargas <otalumi@hotmail.com>
2022-01-30 10:56:28 -0600

    new product y edit product reciben datos desde redux/adminProductActions, aun falla el envio a back

--- client/src/redux/actions/adminProductsActions.js
+++ client/src/redux/actions/adminProductsActions.js
@@ -2,11 +2,30 @@ import { POST_NEW_ARTWORK, GET_ARTWORK, PUT_ARTWORK } from "../types";
 
 export const getArtwork =(id)=>{
   return async function(dispatch){
-    const response = await fetch(`http://localhost:5040/home/${id}`)
+    const response = await fetch(`http://localhost:5040/artwork/${id}`)
     const json = await response.json()
+    console.log(json)
     dispatch({
       type: GET_ARTWORK,
       payload:json
+    })
+  }
+}
+
+export const postNewArtwork = (info) =>{
+  return async function(dispatch){
+    console.log(info)
+    const op = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(info) 
+    }
+    const response = await fetch(`http://localhost:5040/artwork/post`, op)
+    const json = await response.json()
+    console.log(json)
+    dispatch({
+      type:POST_NEW_ARTWORK,
+      payload: json
     })
   }
 }
