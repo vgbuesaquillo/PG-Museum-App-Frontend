6e83d7e8bf115bb03a74b96638ea65bc4f032e5d
Victor Buesaquillo <vicbfisico@gmail.com>
2022-01-31 21:57:49 -0500

    Refactor(components redux) all categories

--- client/src/redux/actions/galleryActions.js
+++ client/src/redux/actions/galleryActions.js
@@ -1,4 +1,5 @@
-import { GET_ALL_GALLERY, GET_FIND_GALLERY, CATEGORIES, SORT_GALLERY } from '../types'
+import { GET_ALL_GALLERY, GET_FIND_GALLERY, CATEGORIES, SORT_GALLERY, TYPES } from '../types'
+import axios from 'axios'
 const url = process.env.REACT_APP_URL
 
 //fetches the list of artworks - recibe la lista de piezas de arte
@@ -35,6 +36,18 @@ export function categories(category) {
         });
     }
 }
+export function categoriesTypes() {
+    return async function dispatch(dispatch) {
+        const response = await axios.get(`${url}/types`);
+        const json = await response.data;
+        dispatch({
+            type: TYPES,
+            payload: json
+        });
+    }
+}
+
+
 
 //manda allGallery ordenado al reducer para actualizar el store
 export function sortGallery(sortedGallery) {
