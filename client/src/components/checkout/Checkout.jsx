c293ce63d44d2f1ddd58a761d470e0fc005d48b2
mariorocha23 <virgoroch852@gmail.com>
2022-02-06 18:45:38 -0600

    add(component checkout) confirm user buy

--- client/src/components/checkout/Checkout.jsx
+++ client/src/components/checkout/Checkout.jsx
@@ -1,24 +1,35 @@
-// import { NavLink } from 'react-router-dom'
-const Checkout = () => {
-    // function handleInputChange() {
+import React, { useState } from 'react'
+import {useSelector} from 'react-redux'
+import '../styles/checkout.css'
 
-    // }
-    return (<div className="maincontainer">
-        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-            <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
-            <nav class="my-2 my-md-0 mr-md-3">
-                {/* <NavLink>Features </NavLink>
-                <NavLink>Enterprise </NavLink>
-                <NavLink>Support </NavLink>
-                <NavLink>Pricing </NavLink> */}
-            </nav>
-            <button class="btn btn-outline-primary" href="#">Sign up</button>
-        </div>
+const Checkout = () => {
+
+    const user = localStorage?.session ? JSON.parse(localStorage.session) : null
+    const total = useSelector(state => state.allProductsReducer.totalCount)
+    const products = useSelector(state => state.allProductsReducer.allproducts)
+
+    const [state , setState] = useState({
+        name: '',
+        username: '',
+        email: '',
+        address: '',
+        country: '',
+        state: '',
+
+    })
+
+    const handleInputChange = (e) => {
+        setState({...state, [e.target.name]: e.target.value})
+    }
+
+    console.log(state);
+
+
+return (
+    <div className="maincontainer w-100">
         <div class="container">
             <div class="py-5 text-center">
-
-                <h2>Checkout form</h2>
-                <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h2>Checkout buy</h2>
             </div>
             <div class="row">
                 <div class="col-md-4 order-md-2 mb-4">
@@ -27,101 +38,68 @@ const Checkout = () => {
                         <span class="badge badge-secondary badge-pill">3</span>
                     </h4>
                     <ul class="list-group mb-3">
-                        <li class="list-group-item d-flex justify-content-between lh-condensed">
-                            <div>
-                                <h6 class="my-0">Product name</h6>
-                                <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$12</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-condensed">
-                            <div>
-                                <h6 class="my-0">Second product</h6>
-                                <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$8</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-condensed">
-                            <div>
-                                <h6 class="my-0">Third item</h6>
-                                <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$5</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between bg-light">
-                            <div class="text-success">
-                                <h6 class="my-0">Promo code</h6>
-                                <small>EXAMPLECODE</small>
-                            </div>
-                            <span class="text-success">-$5</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between">
-                            <span>Total (USD)</span>
-                            <strong>$20</strong>
-                        </li>
+                    <div className='products-scroll'>
+                        {
+                            products.length !== 0 ? products.map(product => (
+                                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                    <div>
+                                        <h6 class="my-0">{product?.title}</h6>
+                                        <small class="text-muted">{product?.creators_description}</small>
+                                    </div>
+                                    <span class="text-muted">${product?.price}</span>
+                                </li>
+                            )) : 
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">No items in store</h6>
+                                    <small class="text-muted">try to add products for buy</small>
+                                </div>
+                            </li>
+                        }
+                    </div>
+                    <li class="list-group-item d-flex justify-content-between">
+                        <span>Total (USD)</span>
+                        <strong>${total}</strong>
+                    </li>
                     </ul>
-                    <form class="card p-2">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Promo code" />
-                            <div class="input-group-append">
-                                <button type="button" class="btn btn-secondary">Redeem</button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
                 <div class="col-md-8 order-md-1">
                     <h4 class="mb-3">Billing address</h4>
                     <form class="needs-validation" novalidate>
                         <div class="row">
                             <div class="col-md-6 mb-3">
-                                <label for="firstName">First name</label>
-                                <input type="text" class="form-control" id="firstName" placeholder="" value="" required />
+                                <label for="firstName">Full name</label>
+                                <input type="text" class="form-control" id="firstName" placeholder="" required onChange={handleInputChange} name='name'/>
                                 <div class="invalid-feedback">
                                     Valid first name is required.
                                 </div>
                             </div>
                             <div class="col-md-6 mb-3">
-                                <label for="lastName">Last name</label>
-                                <input type="text" class="form-control" id="lastName" placeholder="" value="" required />
+                                <label for="lastName">Username</label>
+                                <input type="text" class="form-control" id="lastName" placeholder="" required onChange={handleInputChange} name='username'/>
                                 <div class="invalid-feedback">
-                                    Valid last name is required.
+                                    Valid last Username is required.
                                 </div>
                             </div>
                         </div>
                         <div class="mb-3">
-                            <label for="username">Username</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">@</span>
-                                </div>
-                                <input type="text" class="form-control" id="username" placeholder="Username" required />
-                                <div class="invalid-feedback">
-                                    Your username is required.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="email">Email <span class="text-muted">(Optional)</span></label>
-                            <input type="email" class="form-control" id="email" placeholder="you@example.com" />
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" placeholder="you@example.com" onChange={handleInputChange} name='email'/>
                             <div class="invalid-feedback">
                                 Please enter a valid email address for shipping updates.
                             </div>
                         </div>
                         <div class="mb-3">
                             <label for="address">Address</label>
-                            <input type="text" class="form-control" id="address" placeholder="1234 Main St" required />
+                            <input type="text" class="form-control" id="address" placeholder="1234 Main St" required onChange={handleInputChange} name='address'/>
                             <div class="invalid-feedback">
                                 Please enter your shipping address.
                             </div>
                         </div>
-                        <div class="mb-3">
-                            <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
-                            <input type="text" class="form-control" id="address2" placeholder="Apartment or suite" />
-                        </div>
                         <div class="row">
                             <div class="col-md-5 mb-3">
                                 <label for="country">Country</label>
-                                <select class="custom-select d-block w-100" id="country" required>
+                                <select class="custom-select d-block w-100" id="country" required onChange={handleInputChange} name='country'>
                                     <option value="">Choose...</option>
                                     <option>United States</option>
                                 </select>
@@ -131,77 +109,12 @@ const Checkout = () => {
                             </div>
                             <div class="col-md-4 mb-3">
                                 <label for="state">State</label>
-                                <select class="custom-select d-block w-100" id="state" required>
+                                <select class="custom-select d-block w-100" id="state" required onChange={handleInputChange} name='state'>
                                     <option value="">Choose...</option>
                                     <option>California</option>
                                 </select>
                                 <div class="invalid-feedback">
                                     Please provide a valid state.
-                                </div>
-                            </div>
-                            <div class="col-md-3 mb-3">
-                                <label for="zip">Zip</label>
-                                <input type="text" class="form-control" id="zip" placeholder="" required />
-                                <div class="invalid-feedback">
-                                    Zip code required.
-                                </div>
-                            </div>
-                        </div>
-                        <hr class="mb-4" />
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="same-address" />
-                            <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-                        </div>
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="save-info" />
-                            <label class="custom-control-label" for="save-info">Save this information for next time</label>
-                        </div>
-                        <hr class="mb-4" />
-                        <h4 class="mb-3">Payment</h4>
-                        <div class="d-block my-3">
-                            <div class="custom-control custom-radio">
-                                <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required />
-                                <label class="custom-control-label" for="credit">Credit card</label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required />
-                                <label class="custom-control-label" for="debit">Debit card</label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required />
-                                <label class="custom-control-label" for="paypal">Paypal</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                                <label for="cc-name">Name on card</label>
-                                <input type="text" class="form-control" id="cc-name" placeholder="" required />
-                                <small class="text-muted">Full name as displayed on card</small>
-                                <div class="invalid-feedback">
-                                    Name on card is required
-                                </div>
-                            </div>
-                            <div class="col-md-6 mb-3">
-                                <label for="cc-number">Credit card number</label>
-                                <input type="text" class="form-control" id="cc-number" placeholder="" required />
-                                <div class="invalid-feedback">
-                                    Credit card number is required
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3 mb-3">
-                                <label for="cc-expiration">Expiration</label>
-                                <input type="text" class="form-control" id="cc-expiration" placeholder="" required />
-                                <div class="invalid-feedback">
-                                    Expiration date required
-                                </div>
-                            </div>
-                            <div class="col-md-3 mb-3">
-                                <label for="cc-expiration">CVV</label>
-                                <input type="text" class="form-control" id="cc-cvv" placeholder="" required />
-                                <div class="invalid-feedback">
-                                    Security code required
                                 </div>
                             </div>
                         </div>
@@ -210,14 +123,6 @@ const Checkout = () => {
                     </form>
                 </div>
             </div>
-            <footer class="my-5 pt-5 text-muted text-center text-small">
-                <p class="mb-1">&copy; 2020-2021 therichpost.com</p>
-                <ul class="list-inline">
-                    {/* <li class="list-inline-item"><NavLink>Privacy </NavLink></li>
-                    <li class="list-inline-item"><NavLink>Terms </NavLink></li>
-                    <li class="list-inline-item"><NavLink>Support </NavLink></li> */}
-                </ul>
-            </footer>
         </div>
 
     </div>);
